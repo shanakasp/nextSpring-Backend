8026f3ab44445c48aabf55a4359c7f5537660e61
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-05-22 08:47:24 +0530

    first commit

---
+++ src/main/java/tutorialnextwithspring/entity/UserEntity.java
@@ -0,0 +1,62 @@
+package tutorialnextwithspring.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    
+    private String firstName;
+    private String lastName;
+    private String emailId;
+
+    public UserEntity(long id, String firstName, String lastName, String emailId) {
+        this.id = id;
+        this.emailId = emailId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    // Getter and Setter for id
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    // Getter and Setter for firstName
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    // Getter and Setter for lastName
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    // Getter and Setter for emailId
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+}
---
+++ src/main/java/tutorialnextwithspring/nextspring/NextspringApplication.java
@@ -0,0 +1,13 @@
+package tutorialnextwithspring.nextspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextspringApplication.class, args);
+	}
+
+}
